1b9611ba49de6adc216ec6980128317c4b8c0bfa
Bartosz <kwiatkowsky.kontakt@gmail.com>
2023-06-06 13:20:17 +0200

    work

--- ecb.py
+++ ecb.py
@@ -1,4 +1,24 @@
-from itertools import zip_longest
+import struct
+
+
+def encrypt_ecb(key, plaintext):
+    block_size = len(key)
+    padded_plaintext = pad_data(plaintext, block_size)
+    ciphertext = bytearray()
+    for i in range(0, len(padded_plaintext), block_size):
+        block = padded_plaintext[i:i + block_size]
+        encrypted_block = bytearray(a ^ b for a, b in zip(block, key))
+        ciphertext += encrypted_block
+    return bytes(ciphertext)
+
+def decrypt_ecb(key, ciphertext):
+    block_size = len(key)
+    plaintext = bytearray()
+    for i in range(0, len(ciphertext), block_size):
+        block = ciphertext[i:i + block_size]
+        decrypted_block = bytearray(a ^ b for a, b in zip(block, key))
+        plaintext += decrypted_block
+    return bytes(unpad_data(plaintext))
 
 
 def pad_data(data, block_size):
@@ -6,43 +26,37 @@ def pad_data(data, block_size):
     padding = bytes([padding_length] * padding_length)
     return data + padding
 
-
 def unpad_data(data):
     padding_length = data[-1]
     return data[:-padding_length]
 
 
-def encrypt_ecb(key, plaintext):
-    block_size = len(key)
-    padded_plaintext = pad_data(plaintext, block_size)
-    ciphertext = b""
-    for i in range(0, len(padded_plaintext), block_size):
-        block = padded_plaintext[i:i+block_size]
-        encrypted_block = bytes(a ^ b for a, b in zip(block, key))
-        ciphertext += encrypted_block
-    return ciphertext
+key = b"testtestsecret!"
 
+bmp_file_path = "data/test.bmp"
+with open(bmp_file_path, 'rb') as f:
+    bmp_file_header = f.read(14)
+    bmp_info_header = f.read(40)
+    data = f.read()
 
-def decrypt_ecb(key, ciphertext):
-    block_size = len(key)
-    plaintext = b""
-    for i in range(0, len(ciphertext), block_size):
-        block = ciphertext[i:i+block_size]
-        decrypted_block = bytes(a ^ b for a, b in zip(block, key))
-        plaintext += decrypted_block
-    return unpad_data(plaintext)
+encrypted_data = encrypt_ecb(key, data)
 
+# Save the encrypted data to a BMP file
+encrypted_file_path = "ecb/encrypted.bmp"
+with open(encrypted_file_path, 'wb') as f:
+    f.write(bmp_file_header)
+    f.write(bmp_info_header)
+    f.write(struct.pack('I', len(encrypted_data)))  # Write the size of the data in 4-byte chunks
+    for val in encrypted_data:
+        while val > 0:
+            chunk = val & 0xFFFFFFFF  # Get the lowest 32 bits of the value
+            f.write(struct.pack('!I', chunk))
+            val >>= 32  # Shift the value right by 32 bits
 
-# Key and plaintext
-key = b'SecretKey1234567'
-plaintext = b'This is a secret message'
+decrypted_data = decrypt_ecb(key, encrypted_data)
 
-# Encrypt using ECB
-ciphertext = encrypt_ecb(key, plaintext)
-
-# Decrypt
-decrypted_text = decrypt_ecb(key, ciphertext)
-
-print('Original data:', plaintext)
-print('Encrypted data:', ciphertext)
-print('Decrypted data:', decrypted_text)
+decrypted_file_path = "ecb/decrypted.bmp"
+with open(decrypted_file_path, 'wb') as f:
+    f.write(bmp_file_header)
+    f.write(bmp_info_header)
+    f.write(decrypted_data)
--- rsa.py
+++ rsa.py
@@ -1,55 +1,103 @@
-from PIL import Image
-import numpy as np
-
-from prime_numbers import generate_keypair
+import random
+import struct
 
 
-def encrypt(public_key, plain_text):
-    e, n = public_key
-    encrypted_text = [pow(int(pixel), e, n) for pixel in plain_text]
-    return encrypted_text
+# Miller-Rabin primality test
+def is_prime(n, k=5):
+    if n < 2:
+        return False
+    for p in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]:
+        if n % p == 0:
+            return n == p
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+    for _ in range(k):
+        a = random.randrange(2, n - 1)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
 
-def decrypt(private_key, encrypted_text):
-    d, n = private_key
-    decrypted_text = [pow(int(pixel), d, n) for pixel in encrypted_text]
-    return decrypted_text
 
-def main():
-    public_key, private_key = generate_keypair()
+def generate_large_prime(bits):
+    while True:
+        p = random.getrandbits(bits)
+        if is_prime(p):
+            return p
 
-    # Open BMP image and convert it to a numpy array
-    im = Image.open('all_gray.bmp')
-    pixel_array = np.array(im, dtype=int)  # force all values to int
 
-    # Convert pixel data to 1D numpy array
-    pixel_data_1D = pixel_array.flatten()
+def extended_gcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, y, x = extended_gcd(b % a, a)
+        return g, x - (b // a) * y, y
 
-    # Encrypt the image
-    encrypted_pixel_data = encrypt(public_key, pixel_data_1D)
 
-    # Convert back to original shape
-    encrypted_pixel_array = np.array(encrypted_pixel_data).reshape(pixel_array.shape)
-    
-    # Save the encrypted image
-    encrypted_im = Image.fromarray(encrypted_pixel_array.clip(0, 255).astype(np.uint8)) # clip values to range 0-255 and convert to uint8
-    encrypted_im.save('encrypted.bmp')
-    
-    # Open encrypted image and convert it to a numpy array
-    encrypted_im = Image.open('encrypted.bmp')
-    encrypted_pixel_array = np.array(encrypted_im, dtype=int)  # force all values to int
+def mod_inverse(a, m):
+    g, x, y = extended_gcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
 
-    # Convert pixel data to 1D numpy array
-    encrypted_pixel_data_1D = encrypted_pixel_array.flatten()
 
-    # Decrypt the image
-    decrypted_pixel_data = decrypt(private_key, encrypted_pixel_data_1D)
+def generate_keypair(bits):
+    p = generate_large_prime(bits // 2)
+    q = generate_large_prime(bits // 2)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = 65537
+    d = mod_inverse(e, phi)
+    return (e, n), (d, n)
 
-    # Convert back to original shape
-    decrypted_pixel_array = np.array(decrypted_pixel_data).reshape(pixel_array.shape)
-    
-    # Save the decrypted image
-    decrypted_im = Image.fromarray(decrypted_pixel_array.clip(0, 255).astype(np.uint8)) # clip values to range 0-255 and convert to uint8
-    decrypted_im.save('decrypted.bmp')
 
-if __name__ == "__main__":
-    main()
+def encrypt(pk, plaintext):
+    e, n = pk
+    return [pow(char, e, n) for char in plaintext]
+
+
+def decrypt(sk, ciphertext):
+    d, n = sk
+    return ''.join([chr(pow(char, d, n)) for char in ciphertext])
+
+
+# Generate RSA keys
+KEY_SIZE = 128
+public_key, private_key = generate_keypair(KEY_SIZE)
+
+# Load the encrypted data from the file
+encrypted_file_path = "data/test.bmp"
+with open(encrypted_file_path, 'rb') as f:
+    bmp_file_header = f.read(14)
+    bmp_info_header = f.read(40)
+    data = f.read()
+
+encrypted_data = encrypt(public_key, data)
+
+# Save the encrypted data to a BMP file
+encrypted_file_path = "rsa/encrypted.bmp"
+with open(encrypted_file_path, 'wb') as f:
+    f.write(bmp_file_header)
+    f.write(bmp_info_header)
+    f.write(struct.pack('I', len(encrypted_data)))  # Write the size of the data in 4-byte chunks
+    for val in encrypted_data:
+        while val > 0:
+            chunk = val & 0xFFFFFFFF  # Get the lowest 32 bits of the value
+            f.write(struct.pack('!I', chunk))
+            val >>= 32  # Shift the value right by 32 bits
+
+decrypted_data = decrypt(private_key, encrypted_data)
+decrypted_file_path = "rsa/decrypted.bmp"
+with open(decrypted_file_path, 'wb') as f:
+    f.write(bmp_file_header)
+    f.write(bmp_info_header)
+    f.write(bytes(decrypted_data, 'latin-1'))
