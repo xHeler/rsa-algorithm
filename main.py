f4fc613928182a0984d542f01256a4feb8f7428e
Bartosz <kwiatkowsky.kontakt@gmail.com>
2023-06-06 11:23:28 +0200

    working

--- main.py
+++ main.py
@@ -1,38 +1,99 @@
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.backends import default_backend
+import random
+import struct
 
 
-KEY_SIZE = 2048
+# Miller-Rabin primality test
+def is_prime(n, k=5):
+    if n < 2:
+        return False
+    for p in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]:
+        if n % p == 0:
+            return n == p
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+    for _ in range(k):
+        a = random.randrange(2, n - 1)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
 
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=KEY_SIZE,
-    backend=default_backend()
-)
-public_key = private_key.public_key()
 
-bmp_path = "data/test.bmp"
+def generate_large_prime(bits):
+    while True:
+        p = random.getrandbits(bits)
+        if is_prime(p):
+            return p
 
-with open(bmp_path, 'rb') as f:
+
+def extended_gcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, y, x = extended_gcd(b % a, a)
+        return g, x - (b // a) * y, y
+
+
+def mod_inverse(a, m):
+    g, x, y = extended_gcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+
+def generate_keypair(bits):
+    p = generate_large_prime(bits // 2)
+    q = generate_large_prime(bits // 2)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = 65537
+    d = mod_inverse(e, phi)
+    return (e, n), (d, n)
+
+
+def encrypt(pk, plaintext):
+    e, n = pk
+    return [pow(char, e, n) for char in plaintext]
+
+
+def decrypt(sk, ciphertext):
+    d, n = sk
+    return ''.join([chr(pow(char, d, n)) for char in ciphertext])
+
+
+# Generate RSA keys
+KEY_SIZE = 16
+public_key, private_key = generate_keypair(KEY_SIZE)
+
+# Load the encrypted data from the file
+encrypted_file_path = "data/test.bmp"
+with open(encrypted_file_path, 'rb') as f:
     bmp_file_header = f.read(14)
     bmp_info_header = f.read(40)
     data = f.read()
 
-chunk_size = KEY_SIZE // 8 - 11
-chunks = [data[i:i+chunk_size] for i in range(0, len(data), chunk_size)]
+encrypted_data = encrypt(public_key, data)
 
-encrypted_chunks = [public_key.encrypt(chunk, padding.PKCS1v15()) for chunk in chunks]
-encrypted = b''.join(encrypted_chunks)
-
-with open("encrypted_image.bmp", 'wb') as f:
+encrypted_file_path = "encrypted.bmp"
+with open(encrypted_file_path, 'wb') as f:
     f.write(bmp_file_header)
     f.write(bmp_info_header)
-    f.write(encrypted)
+    f.write(struct.pack('I', len(encrypted_data) * 4))  # Write the size of the data in bytes
+    for val in encrypted_data:
+        f.write(struct.pack('I', val))
 
-decrypted_chunks = [private_key.decrypt(chunk, padding.PKCS1v15()) for chunk in encrypted_chunks]
-decrypted = b''.join(decrypted_chunks)
-
-with open("decrypted_image.bmp", 'wb') as f:
+decrypted_data = decrypt(private_key, encrypted_data)
+decrypted_file_path = "decrypted.bmp"
+with open(decrypted_file_path, 'wb') as f:
     f.write(bmp_file_header)
     f.write(bmp_info_header)
-    f.write(decrypted)
+    f.write(bytes(decrypted_data, 'latin-1'))
